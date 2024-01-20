320b7f0c13f927e4091d1cf993c6d1ceebfb3199
vludlss <vladislav.mikkoev@yandex.ru>
2024-01-20 03:38:25 +0300

    build: Добавить решение

---
+++ Algorithms.BinarySearch/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms.BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
