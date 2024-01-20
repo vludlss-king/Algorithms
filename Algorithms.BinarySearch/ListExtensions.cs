53e44f4b626ad4ddcbeea3607dfd7f8e6219a643
vludlss <vladislav.mikkoev@yandex.ru>
2024-01-20 03:50:23 +0300

    feat: Добавить пример кода
    Бинарный поиск

---
+++ Algorithms.BinarySearch/ListExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Algorithms.BinarySearch
+{
+    internal static class ListExtensions
+    {
+        public static int? CustomBinarySearch<T>(this IList<T> items, T item)
+            where T : IComparable<T>
+        {
+            var low = 0;
+            var high = items.Count - 1;
+
+            while(low <= high)
+            {
+                var middle = (low + high) / 2;
+                var middleItem = items[middle];
+                if (middleItem.CompareTo(item) == 0)
+                    return middle;
+                if (middleItem.CompareTo(item) == 1)
+                    high = middle - 1;
+                else
+                    low = middle + 1;
+            }
+            return null;
+        }
+    }
+}
--- Algorithms.BinarySearch/Program.cs
+++ Algorithms.BinarySearch/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Algorithms.BinarySearch
+﻿using Algorithms.BinarySearch;
+
+namespace Algorithms.BinarySearch
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var result = list.CustomBinarySearch(8);
+
+            Console.WriteLine($"Позиция элемента в списке: {result?.ToString() ?? "не найдено"}");
+            Console.ReadKey();
         }
+
+        
     }
 }
